3895c338871e5b652c9b4db4ad91540bf59aa873
Ryan Yeske <ryan@ryanyeske.com>
2022-09-11 18:36:28 -0700

    Add apollo and login/logout buttons

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,20 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const CURRENT_USER = gql`
+  query CurrentUser {
+    currentUser {
+      id
+      name
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(CURRENT_USER)
+
+  if (loading) {
+    return <p>loading...</p>
+  }
+
+  if (error) {
+    return <p>error</p>
+  }
+
+  if (data.currentUser) {
+    return (
+      <p>
+        Logged in as {data.currentUser.name} <a href="/auth/logout">logout</a>
+      </p>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="/auth/google"
-        >
-          login
-        </a>
-      </header>
-    </div>
-  );
+    <p>
+      <a href="/auth/google">login</a>
+    </p>
+  )
 }
 
 export default App;
