1ddbbd2354ddc5d129a0ea43a4673b9bb14d8b68
Ryan Yeske <ryan@ryanyeske.com>
2022-09-11 23:31:42 -0700

    grant all to visitor on app_development

--- database/create.sql
+++ database/create.sql
@@ -3,4 +3,5 @@ create database app_development with owner appuser;
 create database app_development_shadow with owner appuser;
 
 create role visitor with password 'visitor' login;
+grant all privileges on database app_development to visitor;
 grant visitor to appuser;
