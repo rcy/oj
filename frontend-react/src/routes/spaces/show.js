244efdc41a58fbf1c2a498faae7479373d5d1173
Ryan Yeske <ryan@ryanyeske.com>
2022-10-17 21:26:08 -0700

    Add comments

--- frontend-react/src/routes/spaces/show.js
+++ frontend-react/src/routes/spaces/show.js
@@ -3,5 +3,9 @@ import { useParams } from 'react-router-dom'
 export default function() {
   const { id: spaceId } = useParams()
 
+  // show the space members
+  // show the space messages
+  // customize the space with colors and images
+
   return <div>{spaceId}</div>
 }
