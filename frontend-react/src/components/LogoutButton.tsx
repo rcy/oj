f2a4185dd08c114a8c4d834fbaed9acf99eff4f1
Ryan Yeske <ryan@ryanyeske.com>
2023-03-18 22:02:31 -0700

    Add confirmation to logged out button + chakra

--- frontend-react/src/components/LogoutButton.tsx
+++ frontend-react/src/components/LogoutButton.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@chakra-ui/react";
 import { logout } from "../util/logout";
 
 export default function LogoutButton() {
-  return <button onClick={logout}>logout</button>;
+  function click() {
+    if (window.confirm('are you sure you want to logout?')) {
+      logout()
+    }
+  }
+  return <Button colorScheme="blue" onClick={click}>logout</Button>;
 }
