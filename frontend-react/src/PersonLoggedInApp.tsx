3984da5796a3b4cbf951153c59320bf21bad8efe
Ryan Yeske <ryan@ryanyeske.com>
2023-03-19 00:35:26 -0700

    Show person card

--- frontend-react/src/PersonLoggedInApp.tsx
+++ frontend-react/src/PersonLoggedInApp.tsx
@@ -1,8 +1,8 @@
 import LogoutButton from "./components/LogoutButton";
-import { Heading, Box, Flex, Spacer, VStack, Container } from '@chakra-ui/react'
+import { Text, Heading, Box, Flex, Spacer, Container, CardBody, Card, Avatar } from '@chakra-ui/react'
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Chat from "./components/Chat";
 import MemberSetProfilePicture from "./routes/member/MemberSetProfilePicture";
+import { useCurrentPersonQuery } from "./generated-types";
 
 export default function PersonLoggedInApp() {
   return (
@@ -43,5 +43,53 @@ function PersonLoggedInAppInner() {
 }
 
 function Main() {
-  return <Link to="/pic">Change Profile Picture</Link>
+  const personQuery = useCurrentPersonQuery({
+    fetchPolicy: "network-only",
+  });
+
+  if (personQuery.loading) {
+    return <div></div>
+  }
+
+  return (
+    <div>
+      <PersonCard
+        name={personQuery.data?.currentPerson?.name}
+        avatarUrl={personQuery.data?.currentPerson?.avatarUrl}
+        username={personQuery.data?.currentPerson?.username || ''}
+        title="me"
+      />
+    </div>
+  )
+}
+
+type PersonCardProps = {
+  username: string
+  name?: string
+  avatarUrl?: string
+  title?: string
+}
+
+function PersonCard(props: PersonCardProps) {
+  return (
+    <Link to="/pic">
+      <Card maxW='xs'>
+        <CardBody>
+          <Flex>
+            <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+              <Avatar
+                size='lg'
+                name={props.name}
+                src={props.avatarUrl}
+              />
+              <Box>
+                <Heading size='lg'>{props.username}</Heading>
+                <Text>{props.title}</Text>
+              </Box>
+            </Flex>
+          </Flex>
+        </CardBody>
+      </Card>
+    </Link>
+  )
 }
