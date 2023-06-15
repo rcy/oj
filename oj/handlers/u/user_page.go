a4afa64874352d2480802b9da1e5cb6e0374d3af
Ryan Yeske <ryan@ryanyeske.com>
2023-06-14 19:28:36 -0700

    Show correct user data on profile page

--- oj/handlers/u/user_page.go
+++ oj/handlers/u/user_page.go
@@ -1,11 +1,15 @@
 package u
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
 	"oj/handlers/layout"
 	"oj/handlers/render"
+	"oj/models/gradients"
 	"oj/models/users"
+
+	"github.com/go-chi/chi/v5"
 )
 
 var t = template.Must(template.ParseFiles(layout.File, "handlers/u/user_page.html"))
@@ -17,12 +21,28 @@ func UserPage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	username := chi.URLParam(r, "username")
+	user, err := users.FindByUsername(username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			render.Error(w, "User not found", 404)
+			return
+		}
+	}
+	ug, err := gradients.UserBackground(user.ID)
+	if err != nil {
+		render.Error(w, err.Error(), 500)
+		return
+	}
+	// override layout gradient to show the page user's not the request user's
+	l.BackgroundGradient = *ug
+
 	d := struct {
 		Layout layout.Data
 		User   users.User
 	}{
 		Layout: l,
-		User:   l.User,
+		User:   *user,
 	}
 
 	render.Execute(w, t, d)
