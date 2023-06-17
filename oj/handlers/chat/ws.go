bd0654a8c0a4c1bf6b844c5124b78e13c43efb12
Ryan Yeske <ryan@ryanyeske.com>
2023-06-16 23:09:11 -0700

    Save chat message to database

--- oj/handlers/chat/ws.go
+++ oj/handlers/chat/ws.go
@@ -3,9 +3,12 @@ package chat
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"oj/handlers/render"
+	"oj/models/messages"
+	"oj/models/users"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/gorilla/websocket"
@@ -32,7 +35,11 @@ func (rs RoomSubscriptions) remove(roomID string, conn *websocket.Conn) {
 
 var subs = RoomSubscriptions{}
 
+var messageTemplate = template.Must(template.New("").Parse(`<div id="chat_room" hx-swap-oob="beforeend"><div>{{.Body}}</div></div>`))
+
 func ChatServer(w http.ResponseWriter, r *http.Request) {
+	user := users.Current(r)
+
 	roomID := chi.URLParam(r, "roomID")
 
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -54,7 +61,7 @@ func ChatServer(w http.ResponseWriter, r *http.Request) {
 
 eventLoop:
 	for {
-		messageType, message, err := conn.ReadMessage()
+		messageType, messageBytes, err := conn.ReadMessage()
 		if err != nil {
 			var closeError *websocket.CloseError
 			if errors.As(err, &closeError) {
@@ -71,25 +78,30 @@ eventLoop:
 			break eventLoop
 		}
 
-		log.Printf("Received: %s", message)
+		log.Printf("Received: %s", messageBytes)
 
-		var obj struct {
-			ChatMessage string `json:"chat_message"`
+		var messageData struct {
+			Body string `json:"chat_message"`
 		}
 
-		json.Unmarshal(message, &obj)
+		json.Unmarshal(messageBytes, &messageData)
 
-		output := fmt.Sprintf(`
-<div id="chat_room" hx-swap-oob="beforeend">
-  <div>%s</div>
-</div>
-`, obj.ChatMessage)
+		message, err := messages.Create(messageData.Body, user.Username)
+		if err != nil {
+			log.Printf("error creating message: %s", err)
+			continue eventLoop
+		}
+
+		outMsg, err := render.ExecuteToBytes(messageTemplate, message)
+		if err != nil {
+			log.Printf("error creating message: %s", err)
+			continue eventLoop
+		}
 
 		for conn := range subs[roomID] {
-			err = conn.WriteMessage(messageType, []byte(output))
+			err = conn.WriteMessage(messageType, outMsg)
 			if err != nil {
 				log.Println("Error during message writing:", err)
-				break
 			}
 		}
 	}
