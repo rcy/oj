5013b0e17f096e18bacfa5aad8f02c1de6255ad4
Ryan Yeske <ryan@ryanyeske.com>
2023-06-25 17:19:48 -0700

    Don't show self in friends list

--- oj/handlers/u/user_index.go
+++ oj/handlers/u/user_index.go
@@ -3,6 +3,7 @@ package u
 import (
 	"html/template"
 	"net/http"
+	"oj/db"
 	"oj/handlers/layout"
 	"oj/handlers/render"
 	"oj/models/users"
@@ -11,13 +12,17 @@ import (
 var uit = template.Must(template.ParseFiles(layout.File, "handlers/u/people.html"))
 
 func UserIndex(w http.ResponseWriter, r *http.Request) {
+	user := users.Current(r)
+
 	l, err := layout.GetData(r)
 	if err != nil {
 		render.Error(w, err.Error(), 500)
 		return
 	}
 
-	allUsers, err := users.FindAll()
+	var allUsers []users.User
+
+	err = db.DB.Select(&allUsers, "select * from users where id <> ?", user.ID)
 	if err != nil {
 		render.Error(w, err.Error(), 500)
 		return
