9aecbb48a242b41fc31c03b339658bd16e183530
Ryan Yeske <ryan@ryanyeske.com>
2023-07-03 19:05:39 -0700

    Wrap SSE

--- handlers/eventsource/SSE.go
+++ handlers/eventsource/SSE.go
@@ -1,5 +1,19 @@
 package eventsource
 
-import "github.com/alexandrevicenzi/go-sse"
+import (
+	"net/http"
+
+	"github.com/alexandrevicenzi/go-sse"
+)
 
 var SSE = sse.NewServer(nil)
+
+type Realtime struct{}
+
+func New() Realtime {
+	return Realtime{}
+}
+
+func (Realtime) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	SSE.ServeHTTP(w, r)
+}
