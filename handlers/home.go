8583bf6dd4d66bec11c84523be4d7f23f9d4bf2b
Ryan Yeske <ryan@ryanyeske.com>
2023-08-27 19:50:46 -0700

    Add button to manage family

--- handlers/home.go
+++ handlers/home.go
@@ -1,18 +1,9 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
-	"oj/models/users"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	user := users.FromContext(ctx)
-
-	if user.IsParent {
-		http.Redirect(w, r, "/parent", http.StatusFound)
-	} else {
-		http.Redirect(w, r, fmt.Sprintf("/u/%d", user.ID), http.StatusFound)
-	}
+	http.Redirect(w, r, "/me", http.StatusFound)
 }
