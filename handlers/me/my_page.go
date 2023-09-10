36bbf89630572b4041f6f9e6f9502ec36330d0a5
Ryan Yeske <ryan@ryanyeske.com>
2023-09-10 07:13:16 -0700

    Remove unused templates

--- handlers/me/my_page.go
+++ handlers/me/my_page.go
@@ -6,10 +6,8 @@ import (
 	"oj/db"
 	"oj/handlers/layout"
 	"oj/handlers/render"
-	"oj/models/gradients"
 	"oj/models/users"
 	"oj/templatehelpers"
-	"sort"
 )
 
 var MyPageTemplate = template.Must(template.New("layout.gohtml").Funcs(templatehelpers.FuncMap).ParseFiles(layout.File, "handlers/me/my_page.gohtml", "handlers/me/card.gohtml"))
@@ -34,87 +32,15 @@ func MyPage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	friends, err := getFriends(l.User.ID)
-	if err != nil {
-		render.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	family, err := getFamily(l.User.ID)
-	if err != nil {
-		render.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
 	d := struct {
-		Layout  layout.Data
-		User    users.User
-		Friends []*Friend
-		Family  []*Friend
+		Layout layout.Data
+		User   users.User
 	}{
-		Layout:  l,
-		User:    l.User,
-		Friends: friends,
-		Family:  family,
+		Layout: l,
+		User:   l.User,
 	}
 
 	render.Execute(w, MyPageTemplate, d)
-}
-
-func getFriends(userID int64) ([]*Friend, error) {
-	var friends []*Friend
-	err := db.DB.Select(&friends, `
-select users.*, fi.b_role role
-from users
-join friends fi on fi.b_id = users.id and fi.a_id = $1
-join friends fo on fo.a_id = users.id and fo.b_id = $1
-where fi.b_role = 'friend'
-`, userID)
-	if err != nil {
-		return nil, err
-	}
-
-	err = addGradients(friends)
-	if err != nil {
-		return nil, err
-	}
-
-	unreads, err := getUnreads(userID)
-	if err != nil {
-		return nil, err
-	}
-
-	addUnreadCounts(friends, unreads)
-
-	return friends, nil
-}
-
-func getFamily(userID int64) ([]*Friend, error) {
-	var family []*Friend
-	err := db.DB.Select(&family, `
-select users.*, fi.b_role role
-from users
-join friends fi on fi.b_id = users.id and fi.a_id = $1
-join friends fo on fo.a_id = users.id and fo.b_id = $1
-where fi.b_role <> 'friend'
-`, userID)
-	if err != nil {
-		return nil, err
-	}
-
-	err = addGradients(family)
-	if err != nil {
-		return nil, err
-	}
-
-	unreads, err := getUnreads(userID)
-	if err != nil {
-		return nil, err
-	}
-
-	addUnreadCounts(family, unreads)
-
-	return family, nil
 }
 
 func getUnreads(userID int64) ([]Unread, error) {
@@ -129,31 +55,4 @@ func getUnreads(userID int64) ([]Unread, error) {
 		return nil, err
 	}
 	return unreads, nil
-}
-
-func addUnreadCounts(friends []*Friend, unreads []Unread) {
-	for _, friend := range friends {
-		for _, unread := range unreads {
-			if unread.SenderID == friend.ID {
-				friend.UnreadCount = unread.Count
-				break
-			}
-		}
-	}
-
-	sort.Slice(friends, func(i, j int) bool {
-		return friends[j].UnreadCount < friends[i].UnreadCount
-	})
-}
-
-func addGradients(friends []*Friend) error {
-	for _, friend := range friends {
-		bg, err := gradients.UserBackground(friend.User.ID)
-		if err != nil {
-			return err
-		}
-
-		friend.GradientCSS = bg.Render()
-	}
-	return nil
 }
