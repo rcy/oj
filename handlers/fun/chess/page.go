2c35bf9036858e86c32df2657a988bafde16b3d6
Ryan Yeske <ryan@ryanyeske.com>
2023-09-17 15:49:14 -0700

    Dynamic board generation

--- handlers/fun/chess/page.go
+++ handlers/fun/chess/page.go
@@ -1,18 +1,14 @@
 package chess
 
 import (
-	"encoding/base64"
-	"fmt"
 	"html/template"
-	"image/color"
+	"log"
 	"net/http"
 	"oj/handlers/layout"
 	"oj/handlers/render"
 	"oj/templatehelpers"
-	"strings"
 
 	"github.com/notnil/chess"
-	"github.com/notnil/chess/image"
 )
 
 var MyPageTemplate = template.Must(template.New("layout.gohtml").Funcs(templatehelpers.FuncMap).ParseFiles(layout.File, "handlers/fun/chess/page.gohtml"))
@@ -28,32 +24,39 @@ func Page(w http.ResponseWriter, r *http.Request) {
 	game := chess.NewGame()
 	pos := game.Position()
 
-	m := pos.Board().SquareMap()
+	squareMap := pos.Board().SquareMap()
+
+	svgPiece := [13]string{
+		"", // empty piece
+		"/assets/chess/wK.svg",
+		"/assets/chess/wQ.svg",
+		"/assets/chess/wR.svg",
+		"/assets/chess/wB.svg",
+		"/assets/chess/wN.svg",
+		"/assets/chess/wP.svg",
+		"/assets/chess/bK.svg",
+		"/assets/chess/bQ.svg",
+		"/assets/chess/bR.svg",
+		"/assets/chess/bB.svg",
+		"/assets/chess/bN.svg",
+		"/assets/chess/bP.svg",
+	}
+
+	var rows [8][8]string
+
 	for i := 0; i < 64; i += 1 {
-		fmt.Println(m[chess.Square(i)])
+		piece := squareMap[chess.Square(i)]
+		rows[i/8][i%8] = svgPiece[piece]
 	}
 
-	boardSVG := new(strings.Builder)
-	perspective := image.Perspective(chess.White)
-	yellow := color.RGBA{255, 0, 0, 1}
-	mark := image.MarkSquares(yellow, chess.D2, chess.E4)
-	white := color.RGBA{255, 255, 255, 1}
-	gray := color.RGBA{120, 120, 120, 1}
-	sqrs := image.SquareColors(white, gray)
-	err = image.SVG(boardSVG, pos.Board(), perspective, mark, sqrs)
-	if err != nil {
-		render.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	b64svg := base64.StdEncoding.EncodeToString([]byte(boardSVG.String()))
+	log.Println(rows)
 
 	d := struct {
-		Layout   layout.Data
-		BoardSrc template.HTMLAttr
+		Layout layout.Data
+		Pieces [8][8]string
 	}{
-		Layout:   l,
-		BoardSrc: template.HTMLAttr(fmt.Sprintf(`src="data:image/svg+xml;base64,%s"`, b64svg)),
+		Layout: l,
+		Pieces: rows,
 	}
 
 	render.Execute(w, MyPageTemplate, d)
--- templatehelpers/templatehelpers.go
+++ templatehelpers/templatehelpers.go
@@ -11,4 +11,7 @@ var FuncMap = template.FuncMap{
 	"fromNow": func(t time.Time) string {
 		return durafmt.Parse(time.Now().Sub(t)).LimitFirstN(1).String()
 	},
+	"odd": func(i, j int) int {
+		return (i + j) % 2
+	},
 }
